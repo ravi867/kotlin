6628c6053d643a8d7469b18513cd4ae25207bd49
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-04-13 12:18:14 +0530

    Create variable.kt

---
+++ variable.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var myVariable:Int =20
+    // var is keyword to declare any new variable to our code.
+    // syntax: var variable_name:datatype= variable value
+    // we can skip the datatype part to declare the variable.
+    // we can change the value of the variable.
+    println("the value of my variable is:$myVariable")
+    // $ is used to print the value of that variable.
+    myVariable =30
+    println("now the updated value is :$myVariable")
+}
