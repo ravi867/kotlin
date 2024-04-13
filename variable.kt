c976cfb5ae72cace88070872f0282982472709fb
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-04-13 12:19:56 +0530

    Update variable.kt

--- variable.kt
+++ variable.kt
@@ -8,4 +8,7 @@ fun main() {
     // $ is used to print the value of that variable.
     myVariable =30
     println("now the updated value is :$myVariable")
+    val age:Int = 30
+    val favno=34
+    println("my age is $age and my favorite number is $favno")
 }
