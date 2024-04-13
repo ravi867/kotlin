443c41c92e26866d9c86902d8b5ed3411de104d4
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-04-13 12:49:54 +0530

    Create airthmatic_operators.kt

---
+++ airthmatic_operators.kt
@@ -0,0 +1,9 @@
+fun main() {
+  var a=20
+  var b=40
+  println("the addition is:${a+b}")
+  println("the subtraction is:${a-b}")
+  println("the multiplication is:${a*b}")
+  println("the divison is:${a/b}")
+  println("the modulus is:${a%b}")
+}
