c7bf818c327f65d1d027b6b202024936b0fe2917
ravi bhatt <112262206+ravi867@users.noreply.github.com>
2024-04-13 12:56:07 +0530

    Create Comparison_operator.kt

---
+++ Comparison_operator.kt
@@ -0,0 +1,12 @@
+fun main() {
+   var a=20
+   var b=90
+    // comparison operator are= ==,!=,<,>,<=,>=
+   if(a>b)
+    {
+        println("a is greater than b")
+    }
+    else{
+        println("b is greater than a")
+    }
+}
